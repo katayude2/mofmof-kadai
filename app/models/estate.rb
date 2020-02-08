208e9ac5beca4083d420ebe5cade686cf535a606
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-02-09 02:16:29 +0900

    Estateモデルにバリデーションを追加

--- app/models/estate.rb
+++ app/models/estate.rb
@@ -1,4 +1,9 @@
 class Estate < ApplicationRecord
   has_many :stations
   accepts_nested_attributes_for :stations, allow_destroy: true
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :rent, presence: true, length: { maximum: 30 }
+  validates :address, presence: true, length: { maximum: 140 }
+  validates :age, presence: true, length: { maximum: 30 }
+  validates :note, length: { maximum: 140 }
 end
